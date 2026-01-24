a96d387c006452bc4f80149fc2d8362302506e8c
ChihHsiang Fu <haneyume@gmail.com>
2026-01-24 12:16:22 +0800

    feat: update migration script and database initialization with schema support

--- apps/server/scripts/migrate-db.ts
+++ apps/server/scripts/migrate-db.ts
@@ -1,10 +1,16 @@
 import { migrate } from "drizzle-orm/pglite/migrator";
 import { db } from "../src/db";
 
-export async function applyMigrations() {
+async function applyMigrations() {
   process.env.DB_TYPE = "pglite";
 
   console.log("🔥 apply migrations");
 
-  await migrate(db, { migrationsFolder: "./src/migrations" });
+  await migrate(db, { migrationsFolder: "./src/db/migrations" });
+
+  console.log("✅ migrations applied");
+
+  process.exit(0);
 }
+
+applyMigrations();
--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,6 +1,7 @@
 import { PGlite } from "@electric-sql/pglite";
 import { drizzle } from "drizzle-orm/pglite";
+import * as schema from "../db/schema/auth-schema";
 
 const client = new PGlite("./pglite-db");
 
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
